81b1b221795f8be0d7f61921c623b4f9b1341d23
souspilot <163805276+souspilot@users.noreply.github.com>
2025-12-05 10:03:04 +0000

    Deploying to gh-pages from @ bisht-harshit/bisht-harshit.github.io@8e71dc7f7d2e56e6eb173b096884a0b1fe3cd9a9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "souspilot/souspilot.github.io",
+    "data-repo": "bisht-harshit/bisht-harshit.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
